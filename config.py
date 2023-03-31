a321829a8545e88790408d468ae37ee0203b3560
Alankrith G <100883843+harshan1996@users.noreply.github.com>
2023-03-31 07:46:23 +0530

    config added

---
+++ config.py
@@ -0,0 +1,4 @@
+PASSWORD = "enter-your-db-password"
+DATABASE_NAME = "test"
+COLLECTION_NAME = "BaggageAI"
+MONGODB_URI = f"mongodb+srv://Harshan:{PASSWORD}@firstcluster.zxdpy3p.mongodb.net/?retryWrites=true&w=majority"
