3315b4d3e8ffc6649ea0c52860b55964bbb2c768
Alankrith G <100883843+harshan1996@users.noreply.github.com>
2023-03-31 07:47:20 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-PASSWORD = "enter-your-db-password"
-DATABASE_NAME = "test"
-COLLECTION_NAME = "BaggageAI"
-MONGODB_URI = f"mongodb+srv://Harshan:{PASSWORD}@firstcluster.zxdpy3p.mongodb.net/?retryWrites=true&w=majority"
